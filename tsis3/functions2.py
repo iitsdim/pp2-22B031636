6b835d4514c57286760444e0c2e79fe649c38294
iitsdim <dimash241@gmail.com>
2023-02-02 11:31:09 +0600

    TSIS3 functions2

---
+++ tsis3/functions2.py
@@ -0,0 +1,114 @@
+movies = [
+    {
+        "name": "Usual Suspects",
+        "imdb": 7.0,
+        "category": "Thriller"
+    },
+    {
+        "name": "Hitman",
+        "imdb": 6.3,
+        "category": "Action"
+    },
+    {
+        "name": "Dark Knight",
+        "imdb": 9.0,
+        "category": "Adventure"
+    },
+    {
+        "name": "The Help",
+        "imdb": 8.0,
+        "category": "Drama"
+    },
+    {
+        "name": "The Choice",
+        "imdb": 6.2,
+        "category": "Romance"
+    },
+    {
+        "name": "Colonia",
+        "imdb": 7.4,
+        "category": "Romance"
+    },
+    {
+        "name": "Love",
+        "imdb": 6.0,
+        "category": "Romance"
+    },
+    {
+        "name": "Bride Wars",
+        "imdb": 5.4,
+        "category": "Romance"
+    },
+    {
+        "name": "AlphaJet",
+        "imdb": 3.2,
+        "category": "War"
+    },
+    {
+        "name": "Ringing Crime",
+        "imdb": 4.0,
+        "category": "Crime"
+    },
+    {
+        "name": "Joking muck",
+        "imdb": 7.2,
+        "category": "Comedy"
+    },
+    {
+        "name": "What is the name",
+        "imdb": 9.2,
+        "category": "Suspense"
+    },
+    {
+        "name": "Detective",
+        "imdb": 7.0,
+        "category": "Suspense"
+    },
+    {
+        "name": "Exam",
+        "imdb": 4.2,
+        "category": "Thriller"
+    },
+    {
+        "name": "We Two",
+        "imdb": 7.2,
+        "category": "Romance"
+    }
+]
+
+
+def f1(movie):
+    return movie["imdb"] > 5.5
+
+
+# print(f1(movies[0]))
+def f2():
+    not_bad_movies = []
+    for x in movies:
+        if x["imdb"] > 5.5:
+            not_bad_movies.append(x)
+    return not_bad_movies
+
+
+# print(f2())
+
+def f3(category, movies):
+    res = []
+    for x in movies:
+        if x["category"] == category:
+            res.append(x)
+    return res
+
+
+# print(f3("Romance"))
+
+def f4(movies):
+    return sum(x["imdb"] for x in movies) / len(movies)
+
+
+# print(f4(movies))
+def f5(category, movies):
+    return f4(f3(category, movies))
+
+
+# print(f5("Thriller", movies))
