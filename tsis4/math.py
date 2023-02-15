f9d182fe438756583c23222e92ccbb9e10a24630
iitsdim <dimash241@gmail.com>
2023-02-15 12:45:02 +0600

    TSIS4 math

---
+++ tsis4/math.py
@@ -0,0 +1,18 @@
+import math
+# 1
+# a = int(input("Input degree: "))
+# print(f'Output radian: {a * math.pi / 180}')
+# 2
+# height = int(input("Height: "))
+# base1 = int(input("Base, first value: "))
+# base2 = int(input("Base, second value: "))
+#
+# print(f"Expected Output: {(base1 + base2) * height / 2}")
+# 3
+# sides = int(input("Input number of sides: "))
+# side_length = int(input("Input the length of a side: "))
+# print(f"The area of the polygon is: {sides * (side_length ** 2) / (4 * math.tan(math.pi / sides))}")
+# 4
+base = int(input('Length of base: '))
+height = int(input('Height of parallelogram: '))
+print(f'Expected Output: {base * height}')
